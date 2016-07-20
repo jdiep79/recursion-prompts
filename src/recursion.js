a49dbdd3f5a988134f27cddc0f6e7eec1ddde59f
jdiep79 <jdiep79@gmail.com>
2016-07-20 01:16:47 -0700

    Pass test for range, exponent, powerOfTwo, reverse, and palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -75,7 +75,15 @@ var sumBelow = function(n) {
 // 6. Get the integers in range (x, y).
 // Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
 var range = function(x, y) {
+  var num = x < y ? 1 : -1;
 
+  if (x === y || x + num === y) {
+    return [];
+  }
+
+  var array = range(x += num, y);
+  array.unshift(x);
+  return array;
 };
 
 // 7. Compute the exponent of a number.
@@ -84,6 +92,11 @@ var range = function(x, y) {
 // Example:  exponent(4,3);  // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp == 0) {
+    return 1;
+  }
+  return exp > 0 ? base * exponent(base, exp - 1) : 
+                   exponent(base, exp + 1) / base;
 };
 
 // 8. Determine if a number is a power of two.
@@ -91,14 +104,41 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if(n === 1 || n === 2){//n = 1 or 2?
+    return true
+  } else if(n < 2) {
+    return false
+  }
+  return powerOfTwo(n / 2);
 };
 
 // 9. Write a function that accepts a string a reverses it.
 var reverse = function(string) {
+  if(string.length === 1) {
+    return string;
+  }
+  return reverse(string.slice(1)) + string[0];
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string[0] === ' ') {
+    string = string.slice(1);
+  }
+
+  if (string[string.length - 1] === ' ') {
+    string = string.slice(-1);
+  }
+  
+  if (string.length < 2) {
+    return true;
+  }
+ 
+  if (string[0].toUpperCase() !== string[string.length - 1].toUpperCase()) {
+    return false;
+  }
+
+  return palindrome(string.slice(1,-1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
